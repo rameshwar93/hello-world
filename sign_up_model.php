7cda0df39f7d73ee4986bfe956d20638fd12209d
rameshwar93 <rameshwer.intersoft@gmail.com>
2015-06-18 13:10:46 +0530

    changes to signup

--- sign_up_model.php
+++ sign_up_model.php
@@ -5,21 +5,7 @@ if (!defined('BASEPATH'))
 
 class Sign_up_model extends CI_Model {
 
-    public function __construct() {
-        parent::__construct();
-    }
-
-    public function insert_data($tablename, $data) {
-        $this->db->insert($tablename, $data);
-    }
-
-    public function get_required_id($select_column, $tablename, $where, $id) {
-        $this->db->select($select_column);
-        $this->db->from($tablename);
-        $this->db->where($where, $id);
-        $query = $this->db->get();
-        return $query->result()[0];
-    }
+    
 
 }
 
